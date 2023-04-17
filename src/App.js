2c6e4bd0b06917a0934feac33a59ac979ebe878b
Aditya Raj <101452242+aditya8Raj@users.noreply.github.com>
2023-04-17 10:04:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Categories from './components/Categories';
 import Navbar from './components/Navbar';
+import { ScrollRestoration } from "react-router-dom";
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollRestoration/>
       <Categories />
     </>
   );
